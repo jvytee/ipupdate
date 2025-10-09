a792bffae708bcd68a70aa2dc301fe024f3a5b9c
jvytee <jvytee@posteo.org>
2025-10-10 00:15:07 +0200

    Use rust toolchain from nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,109 +1,82 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, fenix }:
+  outputs = { self, nixpkgs }:
     let
       eachSystem = nixpkgs.lib.genAttrs systems;
       systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      crossTargets = [
-        "aarch64-unknown-linux-musl"
-        "x86_64-unknown-linux-musl"
-      ];
     in {
       devShells =
-        let
-          extraRustStds = system: targets: map (target: fenix.packages.${system}.targets.${target}.stable.rust-std) targets;
-          toolchain = system: targets:
-            with fenix.packages.${system}; combine ([
-              stable.toolchain
-            ] ++ (extraRustStds system targets));
-        in 
-          eachSystem (system:
-            {
-              default = 
-                with import nixpkgs { inherit system; };
-                mkShell {
-                  nativeBuildInputs = [
-                    (toolchain system crossTargets)
-                    yaml-language-server
-                    pkgsCross.aarch64-multiplatform-musl.pkgsStatic.stdenv.cc
-                    pkgsStatic.stdenv.cc
-                    gh
-                  ];
+        eachSystem (system:
+          {
+            default = 
+              with import nixpkgs { inherit system; };
+              mkShell {
+                nativeBuildInputs = [
+                  cargo
+                  gh
+                  yaml-language-server
+                ];
 
-                  CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER =
-                    let cc = pkgsStatic.stdenv.cc;
-                    in "${cc}/bin/${cc.targetPrefix}cc";
-                  CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER =
-                    let cc = pkgsCross.aarch64-multiplatform-musl.pkgsStatic.stdenv.cc;
-                    in "${cc}/bin/${cc.targetPrefix}cc";
-                  # CARGO_BUILD_RUSTFLAGS = [ "-C" "target-feature=+crt-static" ];
-                  # TARGET_CC = "${CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER}";
-                };
-
-              test =
-                with import nixpkgs { inherit system; };
-                mkShell {
-                  nativeBuildInputs = [ fenix.packages.${system}.stable.minimalToolchain ];
-                };
-            }
-          );
+                CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER =
+                  let cc = pkgsStatic.stdenv.cc;
+                  in "${cc}/bin/${cc.targetPrefix}cc";
+                CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER =
+                  let cc = pkgsCross.aarch64-multiplatform-musl.pkgsStatic.stdenv.cc;
+                  in "${cc}/bin/${cc.targetPrefix}cc";
+                # CARGO_BUILD_RUSTFLAGS = [ "-C" "target-feature=+crt-static" ];
+                # TARGET_CC = "${CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER}";
+              };
+          }
+        );
 
       packages = eachSystem (system:
         {
           default =
             with import nixpkgs { inherit system; };
-            let
-              toolchain =
-                with fenix.packages.${system};
-                combine [ stable.cargo stable.rustc ];
-              rustPlatform = makeRustPlatform { cargo = toolchain; rustc = toolchain; };
-            in
-              rustPlatform.buildRustPackage {
-                pname = "ipupdate";
-                version = "0.3.0";
-                src = self;
-                cargoLock.lockFile = ./Cargo.lock;
-              };
+            rustPlatform.buildRustPackage {
+              pname = "ipupdate";
+              version = "0.3.0";
+              src = self;
+              cargoLock.lockFile = ./Cargo.lock;
+            };
 
           ipupdate-x86_64 =
-            with import nixpkgs { localSystem = system; crossSystem = { system = "x86_64-unknown-linux-musl"; isStatic = true; }; };
-            let
-              toolchain =
-                with fenix.packages.${system};
-                combine [ stable.cargo stable.rustc targets.x86_64-unknown-linux-musl.stable.rust-std ];
-              rustPlatform = makeRustPlatform { cargo = toolchain; rustc = toolchain; };
-            in
-              rustPlatform.buildRustPackage {
-                pname = "ipupdate";
-                version = "0.3.0";
-                src = self;
-                cargoLock.lockFile = ./Cargo.lock;
+            with import nixpkgs {
+              localSystem = system;
+              crossSystem = {
+                system = "x86_64-unknown-linux-musl";
+                isStatic = true;
+                rustc.rustcTarget = "x86_64-unknown-linux-musl";
               };
+            };
+            rustPlatform.buildRustPackage {
+              pname = "ipupdate";
+              version = "0.3.0";
+              src = self;
+              cargoLock.lockFile = ./Cargo.lock;
+            };
 
           ipupdate-aarch64 =
-            with import nixpkgs { localSystem = system; crossSystem = { system = "aarch64-unknown-linux-musl"; isStatic = true; }; };
-            let
-              toolchain =
-                with fenix.packages.${system};
-                combine [ stable.cargo stable.rustc targets.aarch64-unknown-linux-musl.stable.rust-std ];
-              rustPlatform = makeRustPlatform { cargo = toolchain; rustc = toolchain; };
-            in
-              rustPlatform.buildRustPackage {
-                pname = "ipupdate";
-                version = "0.3.0";
-                src = self;
-                cargoLock.lockFile = ./Cargo.lock;
+            with import nixpkgs {
+              localSystem = system;
+              crossSystem = {
+                system = "aarch64-unknown-linux-musl";
+                isStatic = true;
+                rustc.rustcTarget = "aarch64-unknown-linux-musl";
               };
+            };
+            rustPlatform.buildRustPackage {
+              pname = "ipupdate";
+              version = "0.3.0";
+              src = self;
+              cargoLock.lockFile = ./Cargo.lock;
+            };
         }
       );
     };
