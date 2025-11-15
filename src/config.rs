45ef7915ea96622303b39693f3592cfae5fc8174
jvytee <jvytee@posteo.org>
2025-11-15 23:42:16 +0100

    Refactor config file code

--- src/config.rs
+++ src/config.rs
@@ -1,38 +1,42 @@
 use anyhow::Result;
 use base64::{engine::general_purpose::STANDARD as standard_engine, prelude::*};
 use serde::Deserialize;
-use std::fs::File;
-use std::io::prelude::*;
+use std::io::{BufReader, Read};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
-    pub domain: String,
-    pub interface: String,
-    pub ipv4_url: Option<String>,
-    pub dyndns_url: String,
+    pub interface_name: Option<String>,
+    pub icanhazip_url: Option<String>,
+    pub dyndns: DynDns,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DynDns {
+    pub domain_name: String,
+    pub endpoint: String,
     pub basic_auth: Option<Auth>,
     pub query: Query,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Auth {
     pub username: String,
     pub password: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct Query {
     pub ipv4: String,
     pub ipv6: String,
 }
 
 impl Config {
-    pub fn from_file(filename: &str) -> Result<Config> {
-        let mut file = File::open(filename)?;
+    pub fn try_from_read<R: Read>(reader: R) -> Result<Self> {
+        let mut buf_reader = BufReader::new(reader);
         let mut content = String::new();
-
-        let _bytes = file.read_to_string(&mut content)?;
+        let _bytes = buf_reader.read_to_string(&mut content)?;
         let config: Config = toml::from_str(&content)?;
+
         Ok(config)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ mod config;
 mod ipaddrs;
 mod ipsource;
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Error, Result};
 use config::Config;
 use getopts::Options;
 use ipaddrs::IpAddrs;
-use reqwest::{
-    Error,
-    blocking::{Client, Request},
-};
-use std::{collections::HashSet, env, net::IpAddr, process};
+use reqwest::blocking::{Client, Request};
+use std::{collections::HashSet, env, fs, net::IpAddr, process};
 
 const DEFAULT_CONFIG: &str = "/etc/ipupdate/config.toml";
 
@@ -42,17 +39,20 @@ fn try_main() -> Result<()> {
         return Ok(());
     }
 
-    let config_file = matches.opt_str("c").unwrap_or(DEFAULT_CONFIG.to_string());
-    log::info!("Loading config file {config_file}");
-    let config = Config::from_file(&config_file)
-        .with_context(|| format!("Could not load config file {config_file}"))?;
+    let filename = matches.opt_str("c").unwrap_or(DEFAULT_CONFIG.to_string());
+    log::info!("Loading config file {filename}");
+    let config_file = fs::File::open(&filename)?;
+    let config = Config::try_from_read(&config_file)
+        .with_context(|| format!("Could not load config file {filename}"))?;
 
-    let interface = &config.interface;
+    let Some(interface) = &config.interface_name else {
+        return Err(Error::msg("No interface name specified!"));
+    };
     log::info!("Reading current IPv6 addresses of interface {interface}");
     let interface_ips = IpAddrs::from_interface(interface);
 
     let client = Client::new();
-    let ip_addrs = match &config.ipv4_url {
+    let ip_addrs = match &config.icanhazip_url {
         Some(endpoint) => {
             log::info!("Requesting current IPv4 addresses from API {endpoint}");
             let api_ips = IpAddrs::from_api(endpoint)
@@ -67,19 +67,19 @@ fn try_main() -> Result<()> {
         None => interface_ips,
     };
 
-    let domain = &config.domain;
+    let domain = &config.dyndns.domain_name;
     log::info!("Resolving registered IP addresses of domain {domain}");
     let domain_ips =
         IpAddrs::from_domain(domain).with_context(|| format!("Could not resolve {domain}"))?;
 
     if !ip_addrs.is_subset(&domain_ips) {
-        log::info!("Updating IP addresses at {}", &config.dyndns_url);
+        log::info!("Updating IP addresses at {}", &config.dyndns.endpoint);
 
         let request =
             create_request(&client, &config, &ip_addrs).context("Could not create request")?;
         let response = client
             .execute(request)
-            .with_context(|| format!("Could not GET {}", &config.dyndns_url))?;
+            .with_context(|| format!("Could not GET {}", &config.dyndns.endpoint))?;
         let status = response.status();
         log::info!("Got status code {status}. Done.");
     } else {
@@ -89,7 +89,11 @@ fn try_main() -> Result<()> {
     Ok(())
 }
 
-fn create_request(client: &Client, config: &Config, ip_addrs: &IpAddrs) -> Result<Request, Error> {
+fn create_request(
+    client: &Client,
+    config: &Config,
+    ip_addrs: &IpAddrs,
+) -> Result<Request, reqwest::Error> {
     let ipv4 = ip_addrs
         .iter()
         .find(|ip_addr| ip_addr.is_ipv4())
@@ -102,11 +106,12 @@ fn create_request(client: &Client, config: &Config, ip_addrs: &IpAddrs) -> Resul
         .map(|ip_addr| ip_addr.to_string())
         .unwrap_or("::".to_string());
 
-    let mut request_builder = client
-        .get(&config.dyndns_url)
-        .query(&[(&config.query.ipv4, &ipv4), (&config.query.ipv6, &ipv6)]);
+    let mut request_builder = client.get(&config.dyndns.endpoint).query(&[
+        (&config.dyndns.query.ipv4, &ipv4),
+        (&config.dyndns.query.ipv6, &ipv6),
+    ]);
 
-    if let Some(auth) = &config.basic_auth {
+    if let Some(auth) = &config.dyndns.basic_auth {
         request_builder = request_builder.header("Authorization", &auth.to_header());
     }
 
@@ -123,7 +128,7 @@ mod tests {
     use reqwest::blocking::Client;
 
     use crate::{
-        config::{Config, Query},
+        config::{Config, DynDns, Query},
         ipaddrs::IpAddrs,
     };
 
@@ -131,14 +136,16 @@ mod tests {
 
     fn test_request_query(ip_addrs: &IpAddrs, url: &str) {
         let config = Config {
-            domain: "foobar.example".to_string(),
-            interface: "eth0".to_string(),
-            ipv4_url: None,
-            dyndns_url: "https://dyndns.example/".to_string(),
-            basic_auth: None,
-            query: Query {
-                ipv4: "foo".to_string(),
-                ipv6: "bar".to_string(),
+            interface_name: Some("eth0".to_string()),
+            icanhazip_url: None,
+            dyndns: DynDns {
+                domain_name: "foobar.example".to_string(),
+                endpoint: "https://dyndns.example/".to_string(),
+                basic_auth: None,
+                query: Query {
+                    ipv4: "foo".to_string(),
+                    ipv6: "bar".to_string(),
+                },
             },
         };
 
